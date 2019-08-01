d5e84b2b168d8cc66c10ec13a7ce620b7f775164
chengzao <czhlink@163.com>
2019-08-01 10:37:30 +0800

    fix: webpack-cli v

--- webpack4/webpack.config.js
+++ webpack4/webpack.config.js
@@ -1,11 +1,10 @@
 // 获取
 const argv = require("yargs").argv;
 
-// console.log('argv ==> ',argv);
-
 // 获取环境命令，并去除首尾空格
 const parms = (argv.env).replace(/(\s*$)|(^\s*)/ig, "");
 
-console.log("parms ==> ", parms);
+console.log(`\n argv.env parms : ${parms}`);
+console.log(`require file : ./config/webpack.config.${parms}.js \n`);
 
 module.exports = require(`./config/webpack.config.${parms}.js`);
